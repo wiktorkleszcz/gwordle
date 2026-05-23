63c83e03223e11f4fa36a43a269ac089e4380580
Wiktor Kleszcz <124890576+wiktorkleszcz@users.noreply.github.com>
2026-05-23 19:23:40 +0200

    Add files

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="bg-stone-900 p-4 m-4 rounded-2xl">
+            <h1 className="text-white">Test</h1>
+        </div>
+    )
+}
